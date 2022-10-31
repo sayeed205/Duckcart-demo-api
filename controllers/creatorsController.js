34f26bc1cba3d5c7ed4fd3ba24280efa53fff4c9
Sayed Ahmed <sayeed205@gmail.com>
2022-10-31 08:31:32 +0530

    JWT sign up method - using JWT to validate creator sign up

--- controllers/creatorsController.js
+++ controllers/creatorsController.js
@@ -1,4 +1,11 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const Creator = require("../models/creatorModel");
+
+const createJWT = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 // login creator
 const loginCreator = async (req, res) => {
@@ -19,7 +26,10 @@ const signupCreator = async (req, res) => {
   try {
     const creator = await Creator.signup(username, password, confirmPassword);
 
-    res.status(200).json({ username, creator });
+    // create jwt
+    const token = createJWT(creator._id);
+
+    res.status(200).json({ username, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
